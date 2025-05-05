a91327c3aa90facbc94e128dc59503fb9b7f032f
Feliks Khafizov <f_khafizov@protonmail.com>
2025-05-05 15:30:21 +0300

    Update ecosystem.config.cjs to set instances to 1 for improved resource management

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "hocuspocus-server",
       script: "./server.js",
-      instances: "max", // Or set to a number (e.g., 2)
+      instances: "1", // Or set to a number (e.g., 2)
       exec_mode: "cluster", // Optional: use 'fork' or 'cluster'
       env: {
         NODE_ENV: "production",
